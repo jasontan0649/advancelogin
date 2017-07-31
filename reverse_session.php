3fbc5bf50a2f75e9a329b7dad53ab5bb3c2a059a
jasontan0649 <jasontankaiyuan@gmail.com>
2017-07-31 20:57:21 +0800

    Add files via upload

---
+++ reverse_session.php
@@ -0,0 +1,19 @@
+<?php
+include("database.php");
+
+session_start();
+$url = "http://localhost/testing.php";
+
+if(empty($_SESSION['login_user']))
+{
+	$_SESSION['login_user'] = "No data receieved";
+}
+
+$user_check = $_SESSION['login_user'];
+$sql ="SELECT * FROM users WHERE username='".$user_check."'";
+$ses_sql = mysqli_query($con, $sql);
+
+if (mysqli_num_rows($ses_sql)==1) {
+	header ('Location:'. $url);
+}
+?>
